1f440b29689cb786e9c5c3e69e85c7c834e56f44
Raphaël H <github@hraph.fr>
2019-01-14 18:30:12 +0100

    Add tsconfig with declaration

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Server = Eureca.Server;
+import Client = Eureca.Client;
+import Transport = Eureca.Transport;
+import Protocol = Eureca.Protocol;
+
+export {Server, Client, Transport, Protocol}
